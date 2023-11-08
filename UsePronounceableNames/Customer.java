3f81aa06a06382403ef4b6786a19929f75b487af
FithraAditya <fithraaditya@gmail.com>
2023-11-08 08:41:18 +0700

    ubah class customer

--- UsePronounceableNames/Customer.java
+++ UsePronounceableNames/Customer.java
@@ -1,10 +1,9 @@
 package UsePronounceableNames;
 
 public class Customer {
-    class DtaRcrd102 {
-        private Date genymdhms;
-        private Date modymdhms;
-        private final String pszqint = "102";
+        private Date generationTimestamp;
+        private Date modificationTimestamp;
+        private final String recordId = "102";
         /* ... */
-        }
+       
 }
