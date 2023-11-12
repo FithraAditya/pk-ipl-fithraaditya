8d75f05d34815cd0961664ef95b9a09f90466a0b
FithraAditya <fithraaditya@gmail.com>
2023-11-12 16:40:04 +0700

    Ubah class Example Assertion

--- Assertion/Example.java
+++ Assertion/Example.java
@@ -7,10 +7,11 @@ public class Example {
         int age = 14;
         assert age <= 18 : "Cannot Vote";
         System.out.println("The voter's age is " + age);
-        Scanner scanner = new Scanner( System.in );
-        System.out.print("Enter ur age ");
-        int value = scanner.nextInt();
-        assert value>=18:" Not valid";
-        System.out.println("value is "+value);
+        try (Scanner scanner = new Scanner( System.in )) {
+            System.out.print("Enter ur age ");
+            int value = scanner.nextInt();
+            assert value>=18:" Not valid";
+            System.out.println("value is "+value);
+        }
     }
 }
