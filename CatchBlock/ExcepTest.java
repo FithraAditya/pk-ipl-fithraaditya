2bfd8c6995128643b5e8f0d513edf9c4961d327b
FithraAditya <fithraaditya@gmail.com>
2023-11-12 19:37:29 +0700

    Penambahan code pada class ExcepTest

--- CatchBlock/ExcepTest.java
+++ CatchBlock/ExcepTest.java
@@ -11,5 +11,15 @@ public class ExcepTest {
         System.out.println("Exception thrown :" + e);
         }
         System.out.println("Out of the block");
+        try {
+                file = new FileInputStream(fileName);
+                x = (byte) file.read();
+            } catch (IOException i) {
+                i.printStackTrace();
+                return -1;
+            } catch (FileNotFoundException f) { // Not valid! 
+                f.printStackTrace();
+                return -1;
+            }
     }
 }
